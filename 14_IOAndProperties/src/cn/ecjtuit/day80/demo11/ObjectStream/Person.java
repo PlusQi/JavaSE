cf6af4e094605c5ac90c64646c5b3ae4ebc2237e
PlusQi <614671436@qq.com>
2019-11-25 18:30:09 +0800

    IO、Properties

---
+++ 14_IOAndProperties/src/cn/ecjtuit/day80/demo11/ObjectStream/Person.java
@@ -0,0 +1,38 @@
+package cn.ecjtuit.day80.demo11.ObjectStream;
+/*
+* 序列化
+* 和反序列化的时候，会抛出NotSerializableException没有序列化异常将无法使其任何状态序列化和反序列化
+* Serializable接口也叫标记型接口
+*   要进行序列化和反序列化的类必须实现Serializable接口，就会给类添加一个标记
+*   当我们进行序列化和反序列化的时候，就会检测类上是否有这个标记
+*       有：就可以序列化和反序列化
+*       没有：就会抛出NotSerializableException异常
+* */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

8c87252212d0eaf48c5dd64b87d0742add822d5b
PlusQi <614671436@qq.com>
2019-11-25 19:38:37 +0800

    IO、Properties

--- 14_IOAndProperties/src/cn/ecjtuit/day80/demo11/ObjectStream/Person.java
+++ 14_IOAndProperties/src/cn/ecjtuit/day80/demo11/ObjectStream/Person.java
@@ -10,10 +10,28 @@ import java.io.Serializable;
 *   当我们进行序列化和反序列化的时候，就会检测类上是否有这个标记
 *       有：就可以序列化和反序列化
 *       没有：就会抛出NotSerializableException异常
+*
+* static关键字：静态关键字
+*   静态优先于非静态加载到内存中(静态优先于对象进入到内存中)
+*   被static修饰的成员变量不能被序列化的，序列化都是对象
+*   private static int age;
+*   oos.writeObject(new Person("马坤", 21));
+*   Object o = ois.readObject();
+*   Person{name='马坤', age=0}
+*
+* transient关键字：瞬态关键字
+*   被transient修饰成员变量，不能被序列化
+*   private transient int age;
+*   oos.writeObject(new Person("马坤", 21));
+*   Object o = ois.readObject();
+*   Person{name='马坤', age=0}
 * */
 public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String name;
     private int age;
+    //private static int age;
+    //private transient int age;
 
     public Person() {
     }

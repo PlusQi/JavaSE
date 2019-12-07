0514d1fa733c21200dba5f22216e217a2248d83e
PlusQi <614671436@qq.com>
2019-12-07 11:15:51 +0800

    通过类名引用静态成员方法

---
+++ 17_StreamAndMethodReference/src/cn/ecjtuit/day93/demo06/StaticMethodReference/Demo01StaticMethodReference.java
@@ -0,0 +1,30 @@
+package cn.ecjtuit.day93.demo06.StaticMethodReference;
+/*
+* 通过类名引用静态成员方法
+* 类已经存在，静态成员方法也已经存在
+* 就可以通过类名直接引用静态成员方法
+* */
+public class Demo01StaticMethodReference {
+    //定义一个方法，方法的参数传递要计算绝对值的整数，和函数式接口Calcable
+    public static int method(int number, Calcable c){
+        return c.calsAbs(number);
+    }
+
+    public static void main(String[] args){
+        //调用method方法，传递计算绝对值的整数，和Lambda表达式
+        int num = method(-1, (n) -> {
+           //对参数进行绝对值的计算并返回结果
+            return Math.abs(n);
+        });
+        System.out.println(num);
+
+        /*
+         * 方法引用优化Lambda表达式
+         * Math类是存在的
+         * abs计算绝对值的静态方法也是已经存在的
+         * 所以我们可以直接通过类名引用静态方法
+         * */
+        int num2 = method(-2,Math::abs);
+        System.out.println(num2);
+    }
+}

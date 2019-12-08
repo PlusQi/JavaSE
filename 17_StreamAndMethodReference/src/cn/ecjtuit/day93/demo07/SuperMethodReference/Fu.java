dc4ded757a3a9c619113b2509f388e84207b0e7c
PlusQi <614671436@qq.com>
2019-12-08 22:06:40 +0800

    通过类名引用静态成员方法

---
+++ 17_StreamAndMethodReference/src/cn/ecjtuit/day93/demo07/SuperMethodReference/Fu.java
@@ -0,0 +1,11 @@
+package cn.ecjtuit.day93.demo07.SuperMethodReference;
+
+/*
+* 定义父类
+* */
+public class Fu {
+    //定义一个hello的方法
+    public void hello(){
+        System.out.println("hello 我是父类");
+    }
+}

2abfb883348c8a9506a43262d7a88af7acffb70d
PlusQi <614671436@qq.com>
2019-12-08 22:08:50 +0800

    使用super引用类的成员方法

--- 17_StreamAndMethodReference/src/cn/ecjtuit/day93/demo07/SuperMethodReference/Fu.java
+++ 17_StreamAndMethodReference/src/cn/ecjtuit/day93/demo07/SuperMethodReference/Fu.java
@@ -4,7 +4,7 @@ package cn.ecjtuit.day93.demo07.SuperMethodReference;
 * 定义父类
 * */
 public class Fu {
-    //定义一个hello的方法
+    //定义一个hello方法
     public void hello(){
         System.out.println("hello 我是父类");
     }

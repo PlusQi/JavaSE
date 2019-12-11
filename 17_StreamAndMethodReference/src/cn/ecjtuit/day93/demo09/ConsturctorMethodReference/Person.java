a79ed9d30924656c3986f22b9be1a2e3bdf7a255
PlusQi <614671436@qq.com>
2019-12-11 13:02:41 +0800

    类的构造方法引用

---
+++ 17_StreamAndMethodReference/src/cn/ecjtuit/day93/demo09/ConsturctorMethodReference/Person.java
@@ -0,0 +1,20 @@
+package cn.ecjtuit.day93.demo09.ConsturctorMethodReference;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

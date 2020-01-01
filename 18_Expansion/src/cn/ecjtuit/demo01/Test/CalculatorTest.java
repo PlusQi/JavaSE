d3423d6e54c4d5074293b3f51904881c7219de56
PlusQi <614671436@qq.com>
2020-01-01 18:56:14 +0800

    为测试类增加@Before和@After标签

--- 18_Expansion/src/cn/ecjtuit/demo01/Test/CalculatorTest.java
+++ 18_Expansion/src/cn/ecjtuit/demo01/Test/CalculatorTest.java
@@ -2,10 +2,31 @@ package cn.ecjtuit.demo01.Test;
 
 import cn.ecjtuit.demo01.Junit.Calculator;
 import jdk.jfr.StackTrace;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+
+    /*
+    * 初始化方法：
+    *   用于资源申请，所有测试方法在执行之前都会先执行该方法
+    * */
+    @Before
+    public void init() {
+        System.out.println("init...");
+    }
+
+    /*
+    * 释放资源方法：
+    *   在所有测试方法执行完后，都会执行该方法
+    * */
+    @After
+    public void close() {
+        System.out.println("close...");
+    }
+
     /*
     * 测试add方法
     * */
@@ -13,6 +34,7 @@ public class CalculatorTest {
     public void testAdd() {
 //        System.out.println("执行");
         //1.创建Calculator对象
+        System.out.println("testAdd...");
         Calculator c = new Calculator();
         //2.调用add方法
         int result = c.add(1, 2);

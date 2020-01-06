6d42883b58f912e390ba14c1f2f9206dd965586e
PlusQi <614671436@qq.com>
2020-01-06 20:48:25 +0800

    一个简单的注解案例

---
+++ 18_Expansion/src/cn/ecjtuit/demo01/annotation/AnnoDemo01.java
@@ -0,0 +1,21 @@
+package cn.ecjtuit.demo01.annotation;
+
+/*
+* 注解javadoc演示
+*
+* @author PlusQi
+* @version 1.0
+* @since 1.5
+* */
+public class AnnoDemo01 {
+
+    /**
+     * 计算两数的和
+     * @param a 整数
+     * @param b 整数
+     * @return 两数之和
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
